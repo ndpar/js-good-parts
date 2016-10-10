6cbabee761f98ffba38950bcc4a6d2aec522c376
Andrey Paramonov <github@ndpar.com>
2016-10-09 20:54:24 -0400

    Curry and memoization

--- useful.js
+++ useful.js
@@ -1,9 +1,35 @@
+/* Augmenting Types */
+
 Function.prototype.method = function (name, func) {
     if (!this.prototype[name]) {
         this.prototype[name] = func;
     }
     return this;
 };
+
 Number.method('integer', function () {
     return Math[this < 0 ? 'ceil' : 'floor'](this);
 });
+
+Function.method('curry', function () {
+    var slice = Array.prototype.slice,
+        args = slice.apply(arguments),
+        that = this;
+    return function () {
+        return that.apply(null, args.concat(slice.apply(arguments)));
+    };
+});
+
+/* Memoization */
+
+var memoizer = function (memo, fundamental) {
+    var shell = function (n) {
+        var result = memo[n];
+        if (typeof result !== 'number') {
+            result = fundamental(shell, n);
+            memo[n] = result;
+        }
+        return result;
+    };
+    return shell; // recursive closure
+};
