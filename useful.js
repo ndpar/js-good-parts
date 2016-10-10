fe67e7a5d74d8c6d4cb18e28df0e6e6fe08b68df
Andrey Paramonov <github@ndpar.com>
2016-10-09 20:11:50 -0400

    useful.js

---
+++ useful.js
@@ -0,0 +1,9 @@
+Function.prototype.method = function (name, func) {
+    if (!this.prototype[name]) {
+        this.prototype[name] = func;
+    }
+    return this;
+};
+Number.method('integer', function () {
+    return Math[this < 0 ? 'ceil' : 'floor'](this);
+});
