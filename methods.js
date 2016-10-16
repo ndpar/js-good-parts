74f382bbb3df8b6e6b2846dbb36981dc46813a13
Andrey Paramonov <github@ndpar.com>
2016-10-16 12:34:05 -0400

    Transform methods.js into test suite

--- methods.js
+++ methods.js
@@ -1,170 +1,25 @@
-/* Helper functions */
+/* Array sorting */
 
-var println = function (f, value) {
-    document.writeln(f + ": " + JSON.stringify(value))
-}
+function by(...fields) {
 
-/* Array */
-
-{
-    let a = ['a', 'b', 'c']
-    println('join', a.join(''))
-}
-{
-    let a = ['a', 'b', 'c']
-    let b = ['x', 'y', 'z']
-    let c = a.concat(b, true)
-    println('concat', {a, b, c})
-}
-{
-    let a = ['a', 'b', 'c']
-    let b = ['x', 'y', 'z']
-    let c = a.push(b, true)
-    println('push', {a, b, c})
-}
-{
-    let a = ['a', 'b', 'c']
-    let b = a.pop()
-    println('pop', {a, b})
-}
-{
-    let a = ['a', 'b', 'c']
-    let b = a.shift()
-    println('shift', {a, b})
-}
-{
-    let a = ['a', 'b', 'c']
-    let b = a.unshift('?', '@')
-    println('unshift', {a, b})
-}
-{
-    let a = ['a', 'b', 'c']
-    let b = a.reverse()
-    println('reverse', {a, b})
-}
-{
-    let a = ['a', 'b', 'c']
-    let b = a.slice(0, 1)
-    let c = a.slice(1)
-    let d = a.slice(1, 2)
-    let e = a.slice()
-    println('slice', {a, b, c, d, e})
-}
-{
-    let a = ['a', 'b', 'c', 'd'];
-    let r = a.splice(1, 2, 'ache', 'bug', 'pig');
-    println('splice', {a, r})
-}
-{
-    let n = [15, 42, 16, 8, 23, 4]
-    let m = n.sort()
-    println('sort', {n, m})
-
-    let p = [15, 42, 16, 8, 23, 4]
-    let q = p.sort((a, b) => a - b)
-    println('sort', {p, q})
-}
-var by = function (name, ...rest) {
-    return function (o, p) {
+    function compare(fields, o, p) {
+        let name = fields[0]
         if (o && p && typeof o === 'object' && typeof p === 'object') {
             let a = o[name], b = p[name]
             if (a === b) {
-                return rest.length > 0 ? by(rest)() : 0;
+                return fields.length > 0 ? compare(fields.slice(1), o, p) : 0
             }
             if (typeof a === typeof b) {
-                return a < b ? -1 : 1;
+                return a < b ? -1 : 1
             }
-            return typeof a < typeof b ? -1 : 1;
+            return typeof a < typeof b ? -1 : 1
         } else {
             throw {
                 name: 'Error',
                 message: 'Expected an object when sorting by ' + name
-            };
+            }
         }
-    };
-};
-{
-    let s = [
-        {first: 'Joe', last: 'DeRita'},
-        {first: 'Moe', last: 'Howard'},
-        {first: 'Joe', last: 'Besser'},
-        {first: 'Shemp', last: 'Howard'},
-        {first: 'Larry', last: 'Fine'},
-        {first: 'Curly', last: 'Howard'}
-    ]
-    println('sort by first', s.sort(by('first')))
-    //println('sort by first,last', s.sort(by('first', 'last'))) // ???
-}
-
-/* Function */
-
-document.writeln()
-
-{
-    document.writeln(function(){return this.value}.apply({value: 42}))
-
-    let x = function (unused) {
-        return this.value
-    }.bind({value: 666}, 'unused too')
-
-    println('bind', x('unused'))
-}
-{
-    let x = function () {
-        return this.value
-    }.bind({value: 666})
-
-    println('bind', x())
-}
-
-/* Number */
-
-document.writeln()
-
-println('toExponential 0', Math.PI.toExponential(0))
-println('toExponential 2', Math.PI.toExponential(2))
-
-println('toFixed 0', Math.PI.toFixed(0))
-println('toFixed 2', Math.PI.toFixed(2))
-
-println('toPrecision 2', Math.PI.toPrecision(2))
-println('toPrecision 7', Math.PI.toPrecision(7))
-
-println('toString  2', Math.PI.toString(2))
-println('toString  8', Math.PI.toString(8))
-println('toString 16', Math.PI.toString(16))
-println('toString   ', Math.PI.toString())
-
-/* String */
-
-document.writeln()
-
-{
-    let text = '<html><body bgcolor=linen><p>This is <b>bold<\/b>!<\/p><\/body><\/html>'
-    let tags = /[^<>]+|<(\/?)([A-Za-z]+)([^<>]*)>/g
-    let a = text.match(tags)
-    for (let i = 0; i < a.length; i += 1) {
-        document.writeln(('// [' + i + '] ' + a[i]).entityify())
     }
-}
 
-document.writeln()
-
-{
-    let text = 'and in it he says "Any damn fool could'
-    let pos = text.search(/["']/)
-    println('search', {text, pos})
-
-    let a = text.slice(18)
-    let b = text.slice(0, 3)
-    let c = text.slice(-5)
-    let d = text.slice(19, 32)
-    println('slice', {a, b, c, d})
+    return (o, p) => compare(fields, o, p)
 }
-{
-    let ip = '192.168.1.0'
-    document.writeln(ip.split('.'))
-    document.writeln(ip.split(/\./))
-    document.writeln(ip.split(/(\.)/))
-}
-document.writeln(String.fromCharCode(67, 97, 116))
---
+++ spec/MethodsSpec.js
@@ -0,0 +1,253 @@
+describe('Array methods', function() {
+
+    it('join', function() {
+        let a = ['a', 'b', 'c']
+        let b = a.join('')
+
+        expect(b).toBe('abc')
+    })
+
+    it('concat', function() {
+        let a = ['a', 'b', 'c']
+        let b = ['x', 'y', 'z']
+        let c = a.concat(b, true)
+
+        expect(a).toEqual(['a', 'b', 'c'])
+        expect(b).toEqual(['x', 'y', 'z'])
+        expect(c).toEqual(['a', 'b', 'c', 'x', 'y', 'z', true])
+    })
+
+    it('push!', function() {
+        let a = ['a', 'b', 'c']
+        let b = ['x', 'y', 'z']
+        let c = a.push(b, true)
+
+        expect(a).toEqual(['a', 'b', 'c', ['x', 'y', 'z'], true])
+        expect(b).toEqual(['x', 'y', 'z'])
+        expect(c).toBe(5)
+    })
+
+    it('pop! (take from right)', function() {
+        let a = ['a', 'b', 'c']
+        let b = a.pop()
+
+        expect(a).toEqual(['a', 'b'])
+        expect(b).toBe('c')
+    })
+
+    it('shift! (take from left)', function() {
+        let a = ['a', 'b', 'c']
+        let b = a.shift()
+
+        expect(a).toEqual(['b', 'c'])
+        expect(b).toBe('a')
+    })
+
+    it('unshift! (concat to left)', function() {
+        let a = ['a', 'b', 'c']
+        let b = a.unshift('?', '@')
+
+        expect(a).toEqual(['?', '@', 'a', 'b', 'c'])
+        expect(b).toBe(5)
+    })
+
+    it('reverse!', function() {
+        let a = ['a', 'b', 'c']
+        let b = a.reverse()
+
+        expect(a).toEqual(['c', 'b', 'a'])
+        expect(b).toEqual(['c', 'b', 'a'])
+    })
+
+    it('slice', function() {
+        let a = ['a', 'b', 'c']
+        expect(a.slice(0, 1)).toEqual(['a'])
+        expect(a.slice(1)).toEqual(['b', 'c'])
+        expect(a.slice(1, 2)).toEqual(['b'])
+        expect(a.slice()).toEqual(a)
+        expect(a).toEqual(['a', 'b', 'c'])
+    })
+
+    it('splice!', function() {
+        let a = ['a', 'b', 'c', 'd'];
+        let r = a.splice(1, 2, 'ache', 'bug', 'pig');
+
+        expect(a).toEqual(['a', 'ache', 'bug', 'pig', 'd'])
+        expect(r).toEqual(['b', 'c'])
+    })
+
+    it('sort! lexicographic', function() {
+        let n = [15, 42, 16, 8, 23, 4]
+        let m = n.sort()
+
+        expect(n).toEqual([15, 16, 23, 4, 42, 8])
+        expect(m).toEqual([15, 16, 23, 4, 42, 8])
+    })
+
+    it('sort! numeric', function() {
+        let p = [15, 42, 16, 8, 23, 4]
+        let q = p.sort((a, b) => a - b)
+
+        expect(p).toEqual([4, 8, 15, 16, 23, 42])
+        expect(q).toEqual([4, 8, 15, 16, 23, 42])
+    })
+})
+
+describe('Array sorting', function() {
+
+    let s = []
+
+    beforeEach(function() {
+        s = [
+            {first: 'Curly', last: 'Howard'},
+            {first: 'Joe', last: 'DeRita'},
+            {first: 'Shemp', last: 'Howard'},
+            {first: 'Joe', last: 'Besser'},
+            {first: 'Moe', last: 'Howard'},
+            {first: 'Larry', last: 'Fine'}
+        ]
+    })
+
+    it('sort! objects by nothing', function() {
+        let unsorted = [
+            {first: 'Curly', last: 'Howard'},
+            {first: 'Joe', last: 'DeRita'},
+            {first: 'Shemp', last: 'Howard'},
+            {first: 'Joe', last: 'Besser'},
+            {first: 'Moe', last: 'Howard'},
+            {first: 'Larry', last: 'Fine'}
+        ]
+        expect(s.sort(by())).toEqual(unsorted)
+    })
+
+    it('sort! objects by first name', function() {
+        let sorted = [
+            {first: 'Curly', last: 'Howard'},
+            {first: 'Joe', last: 'DeRita'},
+            {first: 'Joe', last: 'Besser'},
+            {first: 'Larry', last: 'Fine'},
+            {first: 'Moe', last: 'Howard'},
+            {first: 'Shemp', last: 'Howard'}
+        ]
+        expect(s.sort(by('first'))).toEqual(sorted)
+    })
+
+    it('sort! objects by last name', function() {
+        let sorted = [
+            {first: 'Joe', last: 'Besser'},
+            {first: 'Joe', last: 'DeRita'},
+            {first: 'Larry', last: 'Fine'},
+            {first: 'Curly', last: 'Howard'},
+            {first: 'Shemp', last: 'Howard'},
+            {first: 'Moe', last: 'Howard'}
+        ]
+        expect(s.sort(by('last'))).toEqual(sorted)
+    })
+
+    it('sort! objects by last and first name', function() {
+        let sorted = [
+            {first: 'Joe', last: 'Besser'},
+            {first: 'Joe', last: 'DeRita'},
+            {first: 'Larry', last: 'Fine'},
+            {first: 'Curly', last: 'Howard'},
+            {first: 'Moe', last: 'Howard'},
+            {first: 'Shemp', last: 'Howard'}
+        ]
+        expect(s.sort(by('last', 'first'))).toEqual(sorted)
+    })
+})
+
+describe('Function methods', function() {
+
+    it('apply function with THIS', function() {
+        let v = function(){return this.value}.apply({value: 42})
+        expect(v).toBe(42)
+    })
+
+    it('bind THAT to THIS', function() {
+        let x = function () {
+            return this.value
+        }.bind({value: 666})
+
+        expect(x()).toBe(666)
+    })
+
+    it('bind THAT to THIS (with junk)', function() {
+        let x = function (unused) {
+            return this.value
+        }.bind({value: 666}, {unused: 'junk'})
+
+        expect(x('unused arg')).toBe(666)
+    })
+})
+
+describe('Number methods', function() {
+
+    it('toExponential', function() {
+        expect(Math.PI.toExponential(0)).toBe('3e+0')
+        expect(Math.PI.toExponential(2)).toBe('3.14e+0')
+    })
+
+    it('toFixed', function() {
+        expect(Math.PI.toFixed(0)).toBe('3')
+        expect(Math.PI.toFixed(2)).toBe('3.14')
+    })
+
+    it('toPrecision', function() {
+        expect(Math.PI.toPrecision(2)).toBe('3.1')
+        expect(Math.PI.toPrecision(7)).toBe('3.141593')
+    })
+
+    it('toString', function() {
+        expect(Math.PI.toString(2)).toBe('11.001001000011111101101010100010001000010110100011')
+        expect(Math.PI.toString(8)).toBe('3.1103755242102643')
+        expect(Math.PI.toString(16)).toBe('3.243f6a8885a3')
+        expect(Math.PI.toString()).toBe('3.141592653589793')
+    })
+})
+
+describe('String methods', function() {
+
+    it('match', function() {
+        let text = '<html><body bgcolor=linen><p>This is <b>bold<\/b>!<\/p><\/body><\/html>'
+        let tags = /[^<>]+|<(\/?)([A-Za-z]+)([^<>]*)>/g
+
+        expect(text.match(tags)).toEqual([
+            '<html>',
+            '<body bgcolor=linen>',
+            '<p>',
+            'This is ',
+            '<b>',
+            'bold',
+            '</b>',
+            '!',
+            '</p>',
+            '</body>',
+            '</html>'
+        ])
+    })
+
+    let text = 'and in it he says "Any damn fool could'
+
+    it('search', function() {
+        expect(text.search(/["']/)).toBe(18)
+    })
+
+    it('slice', function() {
+        expect(text.slice(18)).toBe('"Any damn fool could')
+        expect(text.slice(0, 3)).toBe('and')
+        expect(text.slice(-5)).toBe('could')
+        expect(text.slice(19, 32)).toBe('Any damn fool')
+    })
+
+    it('split', function() {
+        let ip = '192.168.1.0'
+        expect(ip.split('.')).toEqual(['192', '168', '1', '0'])
+        expect(ip.split(/\./)).toEqual(['192', '168', '1', '0'])
+        expect(ip.split(/(\.)/)).toEqual(['192', '.', '168', '.', '1', '.', '0'])
+    })
+
+    it('fromCharCode', function() {
+        expect(String.fromCharCode(67, 97, 116)).toBe('Cat')
+    })
+})
