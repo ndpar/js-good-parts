6c8bfa95657adf9423d84ceed40a8e1290797d8a
Andrey Paramonov <github@ndpar.com>
2016-10-18 20:40:46 -0400

    Chapter 10. Beautiful Features

---
+++ src/features.js
@@ -0,0 +1,14 @@
+/*
+ Simplified JavaScript is just the good stuff, including:
+
+ - Functions as first class objects
+ Functions in Simplified JavaScript are lambdas with lexical scoping.
+
+ - Dynamic objects with prototypal inheritance
+ Objects are class-free. We can add a new member to any object by ordinary
+ assignment. An object can inherit members from another object.
+
+ - Object literals and array literals
+ This is a very convenient notation for creating new objects and arrays.
+ JavaScript literals were the inspiration for the JSON data interchange format.
+*/
